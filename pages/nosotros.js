294ccdb19e532fed14913e8f4a652baacca800d5
Clariveljn <cjeldres5@gmail.com>
2023-01-19 14:36:49 -0300

    Agregue página nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Layout from "@/components/layout";
+import styles from '../styles/nosotros.module.css'
 
 export default function Nosotros() {
   return (
@@ -7,7 +9,29 @@ export default function Nosotros() {
         title={"Nosotros"}
         description="Sobre nosotros, guitarLA, Venta de guitarras y blog de música"
       >
-        <h1>Nosotros</h1>
+        <main className="contenedor">
+          <h2 className="heading">Nosotros</h2>
+          <div className={styles.contenido}>
+            <Image 
+            src="/img/nosotros.jpg"
+            width={1000}
+            height={800}
+            alt="Imagen sobre nosotros" />
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur    
+              adipisicing elit. Perspiciatis animi deserunt 
+              incidunt in voluptatem. Maiores quis est ullam 
+              aspernatur veritatis quisquam laborum dolore, rem 
+              corporis ipsam inventore itaque cumque vitae.</p>
+              <p>Lorem ipsum dolor sit amet consectetur, 
+              adipisicing elit. Sed corporis modi illum 
+              molestiae porro veritatis corrupti mollitia 
+              voluptatem. Quis delectus consectetur 
+              excepturi rerum est deserunt obcaecati 
+              pariatur voluptates in facere!</p>
+            </div>
+          </div>
+        </main>
       </Layout>
     </>
   );
