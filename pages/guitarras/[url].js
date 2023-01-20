cb97b63b7f170eb6161c91cf5538d6cfec8089cd
Clariveljn <cjeldres5@gmail.com>
2023-01-19 22:22:01 -0300

    Mostrando información de cadaguitarra

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,6 +1,29 @@
+import Image from "next/image";
+import styles from '../../styles/guitarras.module.css'
+import Layout from "@/components/layout";
+
 export default function Producto({ guitarra }) {
-  console.log(guitarra[0].attributes.nombre);
-  return <div>[url]</div>;
+  const {nombre, descripcion, imagen, precio} = guitarra[0].attributes
+  return (
+    <Layout
+        title={`Guitarra ${nombre}`}
+    >
+    <div className={styles.guitarra}>
+      <Image
+        src={imagen.data.attributes.url}
+        width={600}
+        height={400}
+        alt={`Imagen guitarra ${nombre}`}
+      />
+      <div className={styles.contenido}>
+        <h3>{nombre}</h3>
+        <p className={styles.descripcion}>{descripcion}</p>
+        <p className={styles.precio}>$ {precio}</p>
+      </div>
+    </div>
+    </Layout>
+  )
+  
 }
 
 export async function getStaticPaths() {
